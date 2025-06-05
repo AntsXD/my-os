ca8799c361cfd074d0d503cc656024c31724fbc5
Anthony <anthonycharbel28@gmail.com>
2025-06-05 22:23:18 +0300

    Added code to linker to make a bin file

--- kernel.cpp
+++ kernel.cpp
@@ -8,7 +8,7 @@ void printf(char* str)
 }
 
 
-void kernelMain(void* multiboot_structure, unsigned int magicnumber)
+extern "C" void kernelMain(void* multiboot_structure, unsigned int magicnumber)
 {
     printf("Hello World!---"); // Gonna create a small printf because we cant clal the library when there is no os
 
